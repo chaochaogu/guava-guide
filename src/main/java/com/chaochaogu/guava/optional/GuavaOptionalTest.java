576fc56403b0340cccedf19cdb9b9f71fbbb71bf
gcc <723454695@qq.com>
2019-08-14 00:29:46 +0800

    add

--- src/main/java/com/chaochaogu/guava/optional/GuavaOptionalTest.java
+++ src/main/java/com/chaochaogu/guava/optional/GuavaOptionalTest.java
@@ -3,6 +3,9 @@ package com.chaochaogu.guava.optional;
 import com.google.common.base.Optional;
 
 /**
+ * Optional用于包含非空对象的不可变对象
+ * Optional对象，用于不存在值表示null
+ * 这个类有各种实用的方法，以方便代码来处理为可用或不可用，而不是检查null值
  * @author chaochao gu
  * @date 2019/8/13
  */
